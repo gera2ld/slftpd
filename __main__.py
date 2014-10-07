f5c12b064f7ff6ceee7d60cd8ee79abaf70b5d3b
Gerald <gera2ld@163.com>
2014-10-07 13:26:55 +0800

    Show version info on start
    Signed-off-by: Gerald <gera2ld@163.com>

--- __main__.py
+++ __main__.py
@@ -1,11 +1,12 @@
 #!python
 # coding=utf-8
-import logging,asyncio
+import logging,asyncio,platform
 from . import ftpd,ftpconf
 
 if __name__=='__main__':
 	logging.basicConfig(level=logging.INFO,format='%(asctime)s %(levelname)s: %(message)s')
-	logging.info('FTP Server v2 - by Gerald')
+	logging.info('FTP Server v2/%s %s - by Gerald'
+			% (platform.python_implementation(),platform.python_version()))
 	loop=asyncio.get_event_loop()
 	# TODO: add config file
 	conf=ftpd.FTPHandler.conf=ftpconf.FTPConfig()
